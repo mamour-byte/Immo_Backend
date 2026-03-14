c99b2144a96d2db96a56ba3b14ee47efb05e012c
Mamour <mamourf958@gmail.com>
2026-03-14 09:14:14 +0000

    seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -55,25 +55,36 @@ async function main() {
   const terrasse = await prisma.feature.create({ data: { name: 'Terrasse' } });
 
 
-  const password = await bcrypt.hash('Nadiath123', 10);
+  // Création de trois utilisateurs AGENT
+  const password1 = await bcrypt.hash('Nadiath123', 10);
+  const password2 = await bcrypt.hash('Ami123', 10);
+  const password3 = await bcrypt.hash('Lobe123', 10);
 
-  const existingAdmin = await prisma.user.findUnique({
-    where: { email: 'admin@example.com' },
+  await prisma.user.create({
+    data: {
+      email: 'nadiath@gmail.com',
+      passwordHash: password1,
+      role: 'AGENT',
+      fullName: 'Nadiath Lawogni',
+    },
   });
-
-  if (!existingAdmin) {
-    await prisma.user.create({
-      data: {
-        email: 'nadiath@gmail.com',
-        passwordHash: password,
-        role: 'AGENT',
-        fullName: 'Nadiath Lawognie',
-      },
-    });
-    console.log('Admin user created!');
-  } else {
-    console.log('Admin user already exists!');
-  }
+  await prisma.user.create({
+    data: {
+      email: 'ami@gmail.com',
+      passwordHash: password2,
+      role: 'AGENT',
+      fullName: 'Ami Fall',
+    },
+  });
+  await prisma.user.create({
+    data: {
+      email: 'lobe@gmail.com',
+      passwordHash: password3,
+      role: 'AGENT',
+      fullName: 'Lobe Diop',
+    },
+  });
+  console.log('Trois utilisateurs AGENT créés !');
 }
 
 main()
