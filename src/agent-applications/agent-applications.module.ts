e8c3132016e767af3dc92b3efefd0661317caa92
Mamour <mamourf958@gmail.com>
2026-03-14 10:46:07 +0000

    fix bug

--- src/agent-applications/agent-applications.module.ts
+++ src/agent-applications/agent-applications.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AgentApplicationsController } from './agent-applications.controller';
 import { AgentApplicationsService } from './agent-applications.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AgentApplicationsController],
   providers: [AgentApplicationsService],
 })
 export class AgentApplicationsModule {}
-
